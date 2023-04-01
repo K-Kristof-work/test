dede7a96d948ec57e38b74960fd7bbf7a8e6ae15
Domozi Barnabás <wbqpk3@inf.elte.hu>
2023-04-01 21:49:42 +0200

    Added GameData, Block, Field.

---
+++ Assets/Model/Data/Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Model.Data
+{
+    class Time
+    {
+        DateTime date;
+        int speed;
+
+        // TODO add functions
+    }
+}
---
+++ Assets/Model/Data/Vec2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Model.Data
+{
+    public struct Vec2
+    {
+        public uint x;
+        public uint y;
+
+        public Vec2(uint x, uint y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
