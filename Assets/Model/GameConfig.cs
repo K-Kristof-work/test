801ac96fd4ece6f13c1b16148bbbc66e750fffa7
Domozi Barnabás <wbqpk3@inf.elte.hu>
2023-04-01 21:50:31 +0200

    Getting started with GameModel.

---
+++ Assets/Model/GameConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Model.Data;
+
+namespace Assets.Model
+{
+    static class GameConfig
+    {
+        public static BlockType[] Buildings = { BlockType.PoliceStation, BlockType.Stadium, BlockType.School, BlockType.University, BlockType.Forest, BlockType.PowerPlant, BlockType.PowerLine };
+
+    }
+}
