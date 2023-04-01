a4a051586d1cfa163dc71763baddfc23f06918de
Domozi Barnabás <wbqpk3@inf.elte.hu>
2023-04-01 21:48:25 +0200

    Added PlayerAction class.

---
+++ Assets/Model/PlayerAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Model.Data;
+
+namespace Assets.Model
+{
+    class PlayerAction
+    {
+        private GameData data;
+
+        public PlayerAction (GameData gd)
+        {
+            data = gd;
+        }
+    }
+}
