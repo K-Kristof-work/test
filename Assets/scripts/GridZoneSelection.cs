6a0348384edccb1a346654284657cff82447a3a6
Domozi Barnabás <wbqpk3@inf.elte.hu>
2023-05-07 23:00:44 +0200

    zone selection - render line in view

--- Assets/scripts/GridZoneSelection.cs
+++ Assets/scripts/GridZoneSelection.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Model;
 using Assets.Model.Data;
 
 public class GridZoneSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SelectionBox selectionBox;
 
     private Camera mainCamera;
     private GameData gameData;
+    private PlayerAction playerAction;
     private GameView gameView;
 
     void Start()
     {
         gameData = GameModel.instance.gameData;
+        playerAction = GameModel.instance.playerAction;
         gameView = GetComponent<GameView>();
         mainCamera = Camera.main;
+
+        playerAction.OnZoneSelected += HandleZoneSelected;
+
+        UnityThread.initUnityThread();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
+    {
+        playerAction.OnZoneSelected -= HandleZoneSelected;
+    }
+
+        // Update is called once per frame
+        void Update()
     {
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) // Left mouse button down or held down
         {
@@ -39,6 +51,16 @@ public class GridZoneSelection : MonoBehaviour
         int x = Mathf.FloorToInt((worldPosition.x - transform.position.x) / gameView.CellWidth + gameView.CellWidth / 2);
         int z = Mathf.FloorToInt((worldPosition.z - transform.position.z) / gameView.CellHeight + gameView.CellHeight / 2);
 
-        Debug.Log("zoneid at " + x + " " + z + " = " + gameData.getZoneId(x,z).ToString());
+        //Debug.Log("zoneid at " + x + " " + z + " = " + gameData.getZoneId(x,z).ToString());
+        playerAction.SelectZone(x, z);
+    }
+
+    private void HandleZoneSelected(int tx, int tz, int bx, int bz)
+    {
+        float w = gameView.CellWidth / 2;
+        float h = gameView.cellHeight / 2;
+
+        selectionBox.UpdateSelectionBox(new Vector3(tx - w, 0, tz - h), new Vector3(bx + w, 0, bz + h));
+        selectionBox.SetVisible(true);
     }
 }
